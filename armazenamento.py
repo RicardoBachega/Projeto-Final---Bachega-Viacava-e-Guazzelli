97e6ff16fea1a74ad49521864a3ef5955a0c9d0e
FelipeViacava <fevifrei@hotmail.com>
2016-05-13 08:48:07 -0300

    adicionei email e armazenamento

---
+++ armazenamento.py
@@ -0,0 +1,53 @@
+import pickle
+
+# base_dir =  r"C:\Users\B155 FIRE V3\Desktop\Escola\2016\Design de Software\Projeto Final"
+def armazena(dicionario_comidas, dicionario_bebidas, dicionario_convidados, base_dir):
+    
+    filename = base_dir + r"\arquivo_comidas.dados"
+    fileobj = open(filename, 'wb')    
+    pickle.dump(dicionario_comidas, fileobj)    
+    fileobj.close()
+    
+    filename = base_dir + r"\arquivo_bebidas.dados"
+    fileobj = open(filename, 'wb')
+    pickle.dump(dicionario_bebidas, fileobj)    
+    fileobj.close()
+    
+    filename = base_dir + r"\arquivo_convidados.dados"
+    fileobj = open(filename, 'wb')    
+    pickle.dump(dicionario_convidados, fileobj)    
+    fileobj.close()
+
+def leitura(base_dir):
+    
+    filename = base_dir + r"\arquivo_convidados.dados"
+    fileobj = open(filename, 'rb')
+    convidados = pickle.load(fileobj)
+    
+    filename = base_dir + r"\arquivo_comidas.dados"
+    fileobj = open(filename, 'rb')
+    comidas = pickle.load(fileobj)  
+    
+    filename = base_dir + r"\arquivo_bebidas.dados"    
+    fileobj = open(filename, 'rb')
+    bebidas = pickle.load(fileobj)
+    
+    return convidados, comidas, bebidas
+    
+def zera_dicionarios(dicionario_comidas, dicionario_bebidas, dicionario_convidados, base_dir):
+    
+    dicionario_comidas = {}
+    dicionario_bebidas = {}
+    dicionario_convidados = {}
+
+    armazena(dicionario_comidas, dicionario_bebidas, dicionario_convidados, base_dir)
+    
+def novo_churrasco(base_dir):
+    dicionario_convidados, dicionario_comidas, dicionario_bebidas = leitura(base_dir)
+    zera_dicionarios(dicionario_comidas, dicionario_bebidas, dicionario_convidados, base_dir)
+    
+    convidados = dicionario_convidados
+    comidas = dicionario_comidas
+    bebidas = dicionario_bebidas
+    
+    return convidados, comidas, bebidas
---
+++ enviaemail.py
@@ -0,0 +1,21 @@
+import smtplib
+
+#     definir email do usuario como: toaddrs  = "exemplo@exemplo.exemplo"
+
+def envia_email(texto, toaddrs):
+    fromaddr = "felipevf@al.insper.edu.br"
+    # Add the From: and To: headers at the start!
+    msg = (texto.format(fromaddr, toaddrs))
+    
+    server = smtplib.SMTP('insper.edu.br')
+    server.set_debuglevel(1)
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+    
+    aviso = "Enviado para", toaddrs
+    
+    return aviso
+
+envia_email("Olá, cara!", "fevifrei@hotmail.com")
+
+print(envia_email("Olá, cara!", "fevifrei@hotmail.com"))
